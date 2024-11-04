757b695c4913262793288d51163d4310da6d4021
Halina Tulchii <halendeihalyna@gmail.com>
2024-11-04 07:37:26 +0200

    checked header

--- src/js/modules/mobile-nav.js
+++ src/js/modules/mobile-nav.js
@@ -4,28 +4,39 @@ function mobileNav() {
 	const nav = document.querySelector('.mobile-nav');
 	const navLinks = document.querySelectorAll('.mobile-nav__list a');
 	const menuText = document.querySelector('.mobilenav-text');
+	const iconCamera = document.querySelector('.icon-camera');
+	const iconClose = document.querySelector('.icon-close');
+	const backdrop = document.querySelector('.mobile-nav__backdrop');
 	// const menuIcon = document.querySelector('.mobilenav-icon');
 
 	navBtn.onclick = function () {
 		nav.classList.toggle('mobile-nav--open');
+		backdrop.classList.toggle('mobile-nav__backdrop--active');
 		// menuIcon.classList.toggle('mobilenav-icon--active');
 		document.body.classList.toggle('no-scroll');
 
 		if (nav.classList.contains('mobile-nav--open')) {
 			menuText.textContent = 'Zamknij';
 			navBtn.classList.add('mobilenav-btn--active');
+			iconCamera.style.visibility = 'none';
+			iconClose.style.display = 'inline';
 		} else {
 			menuText.textContent = 'Menu';
 			navBtn.classList.remove('mobilenav-btn--active');
+			iconCamera.style.display = 'inline';
+			iconClose.style.display = 'none';
 		}
 	};
 
 	navLinks.forEach(link => {
 		link.onclick = function () {
 			nav.classList.remove('mobile-nav--open');
+			backdrop.classList.remove('mobile-nav__backdrop--active');
 			document.body.classList.remove('no-scroll');
 			menuText.textContent = 'Menu';
 			navBtn.classList.remove('mobilenav-btn--active');
+			iconCamera.style.display = 'inline';
+			iconClose.style.display = 'none';
 		};
 	});
 }
